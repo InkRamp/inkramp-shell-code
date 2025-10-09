65d2601ae2b39bd3f322fcca61a18f2944233339
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-10-09 14:17:10 +0530

    fix: issue of first call failing for oauth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,8 @@ export const routes: Routes = [
     {
         path: 'auth-callback',
         loadComponent: () => import('./auth-callback/auth-callback.component').then(m => m.AuthCallbackComponent)
-    }
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
     // {
     //     path: '**',
     //     redirectTo: '', // fallback
