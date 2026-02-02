5282fde855da80406831d3b77d8c14d9237cdc3e
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2026-02-02 10:13:46 +0530

    fix: comment out shared services for demo

--- webpack.config.js
+++ webpack.config.js
@@ -15,20 +15,20 @@ module.exports = withModuleFederationPlugin({
     '@opensourcekd/ng-common-libs': { singleton: true, strictVersion: false, requiredVersion: 'auto' },
   },
 
-  // Expose shared services for MFEs from _temp-shared folder
-  exposes: {
-    './RoleService': './src/_temp-shared/role.service.ts',
-    './DummyDataService': './src/_temp-shared/dummy-data.service.ts',
-    './MfeLoaderService': './src/_temp-shared/mfe-loader.service.ts',
-    './EventBusService': './src/_temp-shared/event-bus.service.ts',
-    './AuthService': './src/_temp-shared/auth.service.ts',
-    './AuthInterceptor': './src/_temp-shared/interceptors/auth.interceptor.ts',
-    './AuthConfig': './src/_temp-shared/config/auth.config.ts',
-    './ApiConfig': './src/_temp-shared/config/api.config.ts',
-    './UserProfileService': './src/_temp-shared/user-profile.service.ts',
-    './Models': './src/_temp-shared/models/roles.model.ts',
-    './DataModels': './src/_temp-shared/models/data.model.ts',
-    './MfeModels': './src/_temp-shared/models/mfe.model.ts',
-  },
+  // // Expose shared services for MFEs from _temp-shared folder
+  // exposes: {
+  //   './RoleService': './src/_temp-shared/role.service.ts',
+  //   './DummyDataService': './src/_temp-shared/dummy-data.service.ts',
+  //   './MfeLoaderService': './src/_temp-shared/mfe-loader.service.ts',
+  //   './EventBusService': './src/_temp-shared/event-bus.service.ts',
+  //   './AuthService': './src/_temp-shared/auth.service.ts',
+  //   './AuthInterceptor': './src/_temp-shared/interceptors/auth.interceptor.ts',
+  //   './AuthConfig': './src/_temp-shared/config/auth.config.ts',
+  //   './ApiConfig': './src/_temp-shared/config/api.config.ts',
+  //   './UserProfileService': './src/_temp-shared/user-profile.service.ts',
+  //   './Models': './src/_temp-shared/models/roles.model.ts',
+  //   './DataModels': './src/_temp-shared/models/data.model.ts',
+  //   './MfeModels': './src/_temp-shared/models/mfe.model.ts',
+  // },
 
 });
