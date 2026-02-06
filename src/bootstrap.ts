3741879d655e1d308ff1094ebb48ffcad80ad4d7
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2026-02-06 11:12:34 +0530

    fix : remove providers

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,7 +5,7 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { authInterceptor } from '@org/core-services';
-import { AuthService, EventBusService } from '@opensourcekd/ng-common-libs';
+// import { AuthService, EventBusService } from '@opensourcekd/ng-common-libs';
 // import { cacheInterceptor } from '@org/core-services';  // Temporarily disabled - not needed for auth testing
 //import { OAuthModule } from 'angular-oauth2-oidc';
 
@@ -20,8 +20,8 @@ export function bootstrap() {
         // withInterceptors([cacheInterceptor]),  // Temporarily disabled
       ),
       // Explicitly provide library services to avoid JIT compilation issues
-      { provide: EventBusService, useClass: EventBusService },
-      { provide: AuthService, useClass: AuthService }
+      // { provide: EventBusService, useClass: EventBusService },
+      // { provide: AuthService, useClass: AuthService }
     ],
   });
 }
