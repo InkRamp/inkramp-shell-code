8a09b11aaf3fd5dd937ca22e15026197e2ea267a
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2026-05-15 14:36:46 +0530

    fix: rectify redirect and logout url for auth0

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -17,8 +17,8 @@ const authService = new AuthService(
     domain: 'dev-26sow24tone5na8a.us.auth0.com',
     clientId: 'ht41H0hORjG2GlwHQPRD5pknSjKHsmEB',//'21DGfAeeidKC4hw10PDx5HcOu1gZZF1s',
     audience: 'https://inkramp',//'https://something', //APP_CONFIG.apiUrl,
-    redirectUri: `${window.location.origin}/shell`,
-    logoutUri: `${window.location.origin}/shell`,
+    redirectUri: window.location.origin,
+    logoutUri: window.location.origin,
     scope: 'openid profile email'
   },
   eventBus,
